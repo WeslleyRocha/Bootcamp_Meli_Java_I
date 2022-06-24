73abf118b8314aaf7a79e13b7304ca1a13a6a866
Weslley Rocha <weslley.rocha@mercadolivre.com>
2022-06-23 23:13:47 -0300

    Exemplo While + For

---
+++ src/Exemplo03/exForDecremente.java
@@ -0,0 +1,11 @@
+package Exemplo03;
+
+public class exForDecremente {
+    public static void main(String[] args) {
+
+        // Valor Inicial //Valor da condição // Adicionar ++
+        for(int contador = 10; contador > 0; contador--) {
+            System.out.println("Contagem Decrescente: " + contador);
+        }
+    }
+}
---
+++ src/Exemplo03/exForIncremente.java
@@ -0,0 +1,11 @@
+package Exemplo03;
+
+public class exForIncremente {
+    public static void main(String[] args) {
+
+        // Valor Inicial //Valor da condição // Adicionar ++
+        for(int contador = 0; contador < 11; contador++) {
+            System.out.println("Contagem " + contador);
+        }
+    }
+}
