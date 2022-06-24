dc66f9287e93f9b0a531d07fdca1e5541d2d558c
Weslley Rocha <weslley.rocha@mercadolivre.com>
2022-06-23 22:36:35 -0300

    Exemplo While

---
+++ src/Exemplo03/exWhile.java
@@ -0,0 +1,13 @@
+package Exemplo03;
+
+public class exWhile {
+    public static void main(String[] args) {
+
+        int contador = 0; // 1. valor inicial
+
+        while( contador < 5 ) {  // 2. condição
+            System.out.println("contagem");
+            contador++; // 3. adicionar +1 no contador
+        }
+    }
+}
