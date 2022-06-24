b8ef552a1d3646754a46b436baa8f7691337304b
Weslley Rocha <weslley.rocha@mercadolivre.com>
2022-06-23 22:24:52 -0300

    Exemplo 02

---
+++ src/Exemplo02/exemplo02.java
@@ -0,0 +1,16 @@
+package Exemplo02;
+
+public class exemplo02 {
+    public static void main(String[] args) {
+        int n = 2;
+
+        if (n < 5){
+            System.out.println("Número < 5");
+        } else {
+            if (n > 5) {
+                System.out.println("Número > 5");
+            }
+            System.out.println("Número = 5");
+        }
+    }
+}
